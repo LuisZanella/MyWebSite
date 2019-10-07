953efda8a74088b9db1693183543d5646f8c1a85
LuisZanella <luiscontreraszane@gmail.com>
2019-10-07 12:33:24 -0500

    production

---
+++ build/precache-manifest.4aab1f4411f501782497f8664ee8f532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d522e4459fffe77a34cd58d83c4cae73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684aab101b32006f1301",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "23d90d5992fbadc10c40",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "684aab101b32006f1301",
+    "url": "/static/js/2.fc81822e.chunk.js"
+  },
+  {
+    "revision": "23d90d5992fbadc10c40",
+    "url": "/static/js/main.ba6dfb17.chunk.js"
+  },
+  {
+    "revision": "ef595db4884056d73a27",
+    "url": "/static/js/runtime-main.1fc5ee4d.js"
+  }
+]);
